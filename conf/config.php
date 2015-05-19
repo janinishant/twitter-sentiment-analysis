69e761628ccf1771ee2104b50566e3405f005739
Nishant Jani <nishantjani10@gmail.com>
2015-05-18 19:06:34 -0700

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -1,8 +1,8 @@
 <?php
 $twitter_api_conf = array(
-    'oauth_access_token' => "68449658-kjdnD1u6ZK6oxRbXPqCum70zAGNgyzmWT5ON65pDr",
-    'oauth_access_token_secret' => "1rXUFM5uoiDRlDYri5W3iIG9MgDvlUsym5JLtfiDHAgo5",
-    'consumer_key' => "ruy2btRs3Zus6POk69mK88IRH",
-    'consumer_secret' => "AW617PGaKdag19FZNZXApOGHAw2S2w7enFmTUbr1Z4LwO0JZV0"
+    'oauth_access_token' => "",
+    'oauth_access_token_secret' => "",
+    'consumer_key' => "",
+    'consumer_secret' => ""
 );
 
